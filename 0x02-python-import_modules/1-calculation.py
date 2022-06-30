ecebc44b60c9b702d11dc5571a6cc1646ed3c746
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2022-06-30 22:22:51 +0100

    Update 1-calculation.py
    Updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
-import calculator_1 as calc
-
 if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    add = calc.add(a, b)
-    sub = calc.sub(a, b)
-    mul = calc.mul(a, b)
-    div = calc.div(a, b)
 
-    print("{:d} + {:d} = {:d}".format(a, b, add))
-    print("{:d} - {:d} = {:d}".format(a, b, sub))
-    print("{:d} * {:d} = {:d}".format(a, b, mul))
-    print("{:d} / {:d} = {:d}".format(a, b, div))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
